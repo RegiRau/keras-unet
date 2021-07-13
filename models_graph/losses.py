20b53bf6a9b6d209bc48d004549e245d6daa3b98
Johannes <johannes.schuele@isys.uni-stuttgart.de>
2021-07-13 17:07:46 +0200

    Minimal working tarining example with synthetic data from Regine

--- models_graph/losses.py
+++ models_graph/losses.py
@@ -5,13 +5,11 @@ from keras_unet import TF
 if TF:
     from tensorflow.keras import backend as K
 else:
-<<<<<<< HEAD
-    from keras import backend as K
-import keras
-=======
     from tensorflow.keras import backend as K
+
+
 import tensorflow.keras as keras
->>>>>>> 34c5323c8ffbff520fec42ed430bf9a9dbba4f78
+
 
 def loss_node_positions(y_true, y_pred):
 
